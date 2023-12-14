945dc9eeefffbd15d38f6b5ef26139fce601a067
k22044 <153164749+k22044@users.noreply.github.com>
2023-12-14 16:07:31 +0900

    北沢直暉の日記

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.KitazawaDiary import KitazawaDiary
 # ↓のリストには、メンバーの各日記が格納されます。 
-diaries = [DiarySample(), ]
-    for d in diaries:
+diaries = [DiarySample(),
+           KitazawaDiary(),
+]
+for d in diaries:
         print("---------------------------------")
         print(d.get_date())
         print(d.get_summary())
