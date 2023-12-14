5fac768ae49cb8fb73ee6064146bf89ef9b44c20
k22027 <k22027kk@aitech.ac.jp>
2023-12-14 21:21:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.NishidaDiary import NishidaDiary
+from diaries.k22035Diary import k22035Diary
+from diaries.yutoDiary import yutoDiary
+from diaries.KitazawaDiary import KitazawaDiary
+from diaries.OyaDiary import OyaDiary
+from diaries.LobsterDiary import LobsterDiary
 from diaries.AoshimaDiary import AoshimaDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。 
