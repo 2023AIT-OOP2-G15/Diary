ef590a4035d2108d2a44a07888de85bd7858a3e8
RPNMKK <153748521+RPNMKK@users.noreply.github.com>
2023-12-14 16:16:25 +0900

    x

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from diaries.DiarySample import DiarySample
+from diaries.k22035Diary import k22035Diary
+
 # ↓のリストには、メンバーの各日記が格納されます。 
-diaries = [DiarySample(), ]
+diaries = [DiarySample(), 
+           k22035Diary()]
     for d in diaries:
         print("---------------------------------")
         print(d.get_date())
