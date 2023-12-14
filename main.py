39667ec324d3eb4cf44a3333ba9f9960d1f83a50
nisidatakuma <153790353+nisidatakuma@users.noreply.github.com>
2023-12-14 16:15:34 +0900

    a

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from diaries.DiarySample import DiarySample
+from diaries.NishidaDiary import NishidaDiary
 # ↓のリストには、メンバーの各日記が格納されます。 
-diaries = [DiarySample(), ]
-    for d in diaries:
+diaries = [
+        DiarySample(), 
+        NishidaDiary(),
+]
+   
+for d in diaries:
         print("---------------------------------")
         print(d.get_date())
         print(d.get_summary())
