adc4d1b5e3c0e703de393da391fb15a54e11b5bc
Lobster <sato4ebi@gmail.com>
2023-12-14 15:49:47 +0900

    胡田日記追加

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.LobsterDiary import LobsterDiary
 # ↓のリストには、メンバーの各日記が格納されます。 
-diaries = [DiarySample(), ]
-    for d in diaries:
-        print("---------------------------------")
-        print(d.get_date())
-        print(d.get_summary())
-        print(d.get_author())
-        print()
+diaries = [DiarySample(),LobsterDiary(), ]
+for d in diaries:
+    print("---------------------------------")
+    print(d.get_date())
+    print(d.get_summary())
+    print(d.get_author())
+    print()
