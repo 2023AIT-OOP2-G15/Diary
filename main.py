7843cdd9411d7e54e8a1bab1634fde3ef94c5ca2
YutoSaeki <0sy@ymail.ne.jp>
2023-12-14 15:52:29 +0900

    佐伯祐人の日記
    2023年12月14日の日記です。

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from diaries.DiarySample import DiarySample
+from diaries.yutoDiary import yutoDiary
 # ↓のリストには、メンバーの各日記が格納されます。 
-diaries = [DiarySample(), ]
-    for d in diaries:
-        print("---------------------------------")
-        print(d.get_date())
-        print(d.get_summary())
-        print(d.get_author())
-        print()
+diaries = [
+    DiarySample(), 
+    yutoDiary(),
+]
+
+for d in diaries:
+    print("---------------------------------")
+    print(d.get_date())
+    print(d.get_summary())
+    print(d.get_author())
+    print()
